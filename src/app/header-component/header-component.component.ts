c09f13861e9590542ce80b7c0a81a1f4fe048cd4
largonet84 <50383657+largonet84@users.noreply.github.com>
2025-04-21 01:09:53 -0300

    Test

--- src/app/header-component/header-component.component.ts
+++ src/app/header-component/header-component.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { CartComponent } from '../components/cart/cart.component';
 
 @Component({
   standalone: true,
   selector: 'app-header-component',
-  imports: [CommonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    CartComponent
+  ],
   templateUrl: './header-component.component.html',
   styleUrls: ['./header-component.component.css']
 })
 export class HeaderComponentComponent {
   isCartOpen = false;
-  items: any[] = []; // tu modelo de carrito
 
   toggleCart() {
     this.isCartOpen = !this.isCartOpen;
-  }
-  closeCart() {
-    this.isCartOpen = false;
   }
 }
