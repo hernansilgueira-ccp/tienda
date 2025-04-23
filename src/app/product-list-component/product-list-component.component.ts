ef0c8a578af319176df0b36238668221f5582173
largonet84 <50383657+largonet84@users.noreply.github.com>
2025-04-22 23:39:04 -0300

    Carrousel 90%

--- src/app/product-list-component/product-list-component.component.ts
+++ src/app/product-list-component/product-list-component.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductoService, Producto } from '../services/product.service';
 import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-list-component.component.html',
   styleUrls: ['./product-list-component.component.css']
 })
